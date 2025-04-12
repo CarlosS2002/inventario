64d52edd1d85221629cd132ab071d60c1e9550e1
CarlosS2002 <106689411+CarlosS2002@users.noreply.github.com>
2025-04-11 21:10:27 -0500

    Update categorias.controllers.js

--- backend/src/controllers/categorias.controllers.js
+++ backend/src/controllers/categorias.controllers.js
@@ -1,9 +1,15 @@
-const getCategorias = (req, res) => {
-  res.json({
-    categoria: "Electrodomésticos"
-  });
-};
+import getConnection from "./../db/database.js"
+const getCategorias = async (req, res)=>{
+    try {
+        const connection = await getConnection();
+        const result = await connection.query("SELECT CategoriaID, CategoriaNombre, Descripcion, Imagen FROM categorias")
+        res.json(result);
+    } catch (error) {
+        console.error("ERROR 500");
+    }
+}
 
-export const metodosHTTP = {
-  getCategorias
-};
+export const methodHTTP = {
+    getCategorias
+     
+}
